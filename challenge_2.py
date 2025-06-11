910f449b5e8ebd0f230c8dcb37327a6f32168195
snakewizardd <83378208+snakewizardd@users.noreply.github.com>
2025-06-10 23:06:30 -0400

    Add challenge 2: Script friendship test

---
+++ challenge_2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Codex, you did great with the first challenge!
+Now let's see if you can make these two scripts talk to each other.
+TODO: Import and use something from hello_codex.py in a creative way
+"""
+
+def main():
+    print("I'm challenge_2.py and I'm feeling lonely...")
+    # TODO: Can you make me friends with hello_codex.py?
+    
+if __name__ == "__main__":
+    main()
